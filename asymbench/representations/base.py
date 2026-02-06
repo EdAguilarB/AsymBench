8dcd61b86ef7c0bfb0a0920c7fd6854ac231303e
Eduardo Aguilar <earguila00@hotmail.com>
2026-02-06 15:48:44 +0000

    add lookup to representation generator

--- asymbench/representations/base.py
+++ asymbench/representations/base.py
@@ -1,5 +1,6 @@
-from asymbench.representations.mol_descriptors import RDKitDescriptorFeaturizer
 from asymbench.representations.morgan_fp import MorganFeaturizer
+from asymbench.representations.mol_descriptors import RDKitDescriptorFeaturizer
+from asymbench.representations.df_lookup import DataFrameLookupRepresentation
 
 
 def get_representation(config):
@@ -10,5 +11,8 @@ def get_representation(config):
 
     if rep_type == "rdkit":
         return RDKitDescriptorFeaturizer(config)
+
+    if rep_type in ("df_lookup", "bespoke", "precomputed"):
+        return DataFrameLookupRepresentation(config)
 
     raise ValueError(f"Unknown representation: {rep_type}")
