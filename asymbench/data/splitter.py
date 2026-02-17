75869d4dd549bceb87814082b6130f3c6fb10da5
Eduardo Aguilar <earguila00@hotmail.com>
2026-02-17 10:51:38 +0000

    implement get_train_test_set

--- asymbench/data/splitter.py
+++ asymbench/data/splitter.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, List
 
+import pandas as pd
+import numpy as np
 from astartes.molecules import train_test_split_molecules
 
 
@@ -26,7 +28,46 @@ class MoleculeSplitter:
         self.train_size = self.config["train_size"]
         self.sampler = self.config["sampler"]
 
-    def get_splits(self, mols, y=None, return_indices: bool = True):
+    def get_train_test_set(
+        self,
+        data: pd.DataFrame,
+        mols: List | pd.Series,
+        y: pd.Series,
+        external_test: pd.DataFrame | None = None,
+    ):
+
+        if external_test is not None:
+
+            missing = [
+                col
+                for col in data.columns.tolist() + [y.name]
+                if col not in external_test.columns
+            ]
+
+            assert (
+                not missing
+            ), f"Missing columns in external test set: {missing}"
+
+            df_test = external_test[data.columns]
+            df_train = data.copy()
+            y_train = y.copy()
+            y_test = external_test[y.name]
+
+        else:
+            train_idxs, test_idxs = self.get_splits(mols, y)
+            df_train = data.iloc[train_idxs]
+            df_test = data.iloc[test_idxs]
+            y_train = y.iloc[train_idxs]
+            y_test = y.iloc[test_idxs]
+
+        return df_train, df_test, y_train, y_test
+
+    def get_splits(
+        self,
+        mols: List | pd.Series,
+        y: pd.DataFrame | np.ndarray | None = None,
+        return_indices: bool = True,
+    ):
         """
         Returns indices (or split objects) using the stored random state.
 
