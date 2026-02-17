e0d60c37f363bde56a0620c2c47575f140408514
Eduardo Aguilar <earguila00@hotmail.com>
2026-02-17 10:50:25 +0000

    drop nas loaded data

--- asymbench/data/loader.py
+++ asymbench/data/loader.py
@@ -5,5 +5,6 @@ def load_dataset(config):
     path = config["path"]
     data = pd.read_csv(path)
     data = data.set_index(config["id_col"])
+    data = data.dropna()
     data = data[config["smiles_columns"] + [config["target"]]]
     return data
