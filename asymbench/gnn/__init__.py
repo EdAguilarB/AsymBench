aa22b79ad31a905f61180294c2c7f1c4bafbfe49
Eduardo Aguilar <earguila00@hotmail.com>
2026-04-03 14:27:12 +0100

    update init GNNs

--- asymbench/gnn/__init__.py
+++ asymbench/gnn/__init__.py
@@ -6,15 +6,30 @@ from asymbench.gnn.featurizer import (
 )
 from asymbench.gnn.reaction_graph import ReactionGraphBuilder, merge_molecular_graphs
 from asymbench.gnn.dataset import ReactionGraphDataset
-from asymbench.gnn.model import ReactionGCN
+from asymbench.gnn.base import BaseReactionGNN
+from asymbench.gnn.architectures import (
+    ReactionGCN,
+    ReactionGAT,
+    ReactionGIN,
+    build_reaction_gnn,
+)
 
 __all__ = [
+    # Featurizer constants
     "NODE_FEAT_DIM",
     "EDGE_FEAT_DIM",
     "mol_to_graph",
     "smiles_to_graph",
+    # Graph builder / dataset
     "merge_molecular_graphs",
     "ReactionGraphBuilder",
     "ReactionGraphDataset",
+    # Base class
+    "BaseReactionGNN",
+    # Architecture subclasses
     "ReactionGCN",
+    "ReactionGAT",
+    "ReactionGIN",
+    # Factory
+    "build_reaction_gnn",
 ]
