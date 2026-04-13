f74943d56f8c15eb5b9fe7f1fb349b1e6f749400
Eduardo Aguilar <earguila00@hotmail.com>
2026-04-13 15:57:08 +0100

    isort

--- asymbench/optimization/optuna_optimizer.py
+++ asymbench/optimization/optuna_optimizer.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
 import gc
 import logging
-from dataclasses import dataclass
 from typing import Any, Dict, Tuple
 
 import numpy as np
